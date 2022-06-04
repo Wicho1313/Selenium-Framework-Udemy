ae10c7d3ca6dc0ad533d2053402ea952b1fa65e5
Luis Enrique Rojas Alvarado <lrojase1@gmail.com>
2022-06-04 16:15:20 -0500

    Adding products to cart on test page

---
+++ SeleniumLearning/AddProductsToCart.cs
@@ -0,0 +1,66 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace SeleniumLearning
+{
+    internal class AddProductsToCart
+    {
+        IWebDriver driver;
+        [SetUp]
+        public void Setup()
+        {
+            TestContext.Progress.WriteLine("Initializing SetUp");
+
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+            driver = new ChromeDriver();
+            // Implicit wait can be declared globaly
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+
+            driver.Manage().Window.Maximize();
+
+            driver.Url = "https://rahulshettyacademy.com/loginpagePractise/";
+        }
+
+        [Test]
+        public void AddingProductsToCart()
+        {
+            String[] expectedProducts = { "iphone X", "BlackBerry" };
+            driver.FindElement(By.Id("username")).SendKeys("rahulshettyacademy");           
+            driver.FindElement(By.Name("password")).SendKeys("learning");
+            driver.FindElement(By.XPath("//div[@class='form-group'][5]/label/span/input")).Click(); 
+            driver.FindElement(By.XPath("//input[@id='signInBtn']")).Click();
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(8));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.PartialLinkText("Checkout")));
+
+            IList<IWebElement> products = driver.FindElements(By.TagName("app-card"));
+
+            foreach(IWebElement product in products)
+            {
+                // displaying list of products on page
+                TestContext.Progress.WriteLine(product.FindElement(By.CssSelector(".card-title a")).Text);
+                // doing matching with the expected on first list
+                if(expectedProducts.Contains(product.FindElement(By.CssSelector(".card-title a")).Text))
+                {
+                    product.FindElement(By.CssSelector(".card-footer button")).Click();
+                }
+            }
+
+        }
+        [TearDown]
+        public void CloseBrowser()
+        {
+            TestContext.Progress.WriteLine("Finishing");
+            driver.Quit();
+        }
+    }
+}
