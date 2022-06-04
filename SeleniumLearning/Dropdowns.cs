3ce6bdb20929498bcb266b725899c8fadc851a15
Luis Enrique Rojas Alvarado <lrojase1@gmail.com>
2022-06-04 15:53:43 -0500

    Adding Dropdowns and Radio buttons handle with Selenium on C# .Net

---
+++ SeleniumLearning/Dropdowns.cs
@@ -0,0 +1,51 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace SeleniumLearning
+{
+    internal class Dropdowns
+    {
+        IWebDriver driver;
+        [SetUp]
+        public void Setup()
+        {
+            TestContext.Progress.WriteLine("Initializing SetUp");
+
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+            driver = new ChromeDriver();
+            // Implicit wait can be declared globaly
+            
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+
+            driver.Manage().Window.Maximize();
+
+            driver.Url = "https://rahulshettyacademy.com/loginpagePractise/";
+        }
+
+        [Test]
+        public void DropDown()
+        {
+            IWebElement dropDown = driver.FindElement(By.CssSelector("select.form-control"));
+            SelectElement selectElement = new SelectElement(dropDown);
+
+            selectElement.SelectByText("Teacher");
+            selectElement.SelectByValue("consult");// 'value' attribute of the HTML element
+            selectElement.SelectByIndex(1);
+
+        }
+        [TearDown]
+        public void CloseBrowser()
+        {
+            TestContext.Progress.WriteLine("Finishing");
+            driver.Quit();
+        }
+    }
+}
---
+++ SeleniumLearning/RadioButton.cs
@@ -0,0 +1,61 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace SeleniumLearning
+{
+    internal class RadioButton
+    {
+        IWebDriver driver;
+        [SetUp]
+        public void Setup()
+        {
+            TestContext.Progress.WriteLine("Initializing SetUp");
+
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+            driver = new ChromeDriver();
+            // Implicit wait can be declared globaly
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+
+            driver.Manage().Window.Maximize();
+
+            driver.Url = "https://rahulshettyacademy.com/loginpagePractise/";
+        }
+
+        [Test]
+        public void radioButton()
+        {
+            IList<IWebElement> radioButtons= driver.FindElements(By.CssSelector("input[id='usertype']"));
+
+            foreach(IWebElement radioButton in radioButtons)
+            {
+                if (radioButton.GetAttribute("value").Equals("user"))
+                {
+                    radioButton.Click();
+                }
+            }
+            var okayButton = driver.FindElement(By.Id("okayBtn"));
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(8));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(okayButton));
+
+            okayButton.Click();
+
+            Boolean result = driver.FindElement(By.Id("usertype")).Selected;// to know if radiobutton was selected - tag must have attribute 'Selected'
+            Assert.That(result, Is.True);
+        }
+        [TearDown]
+        public void CloseBrowser()
+        {
+            TestContext.Progress.WriteLine("Finishing");
+            driver.Quit();
+        }
+    }
+}
