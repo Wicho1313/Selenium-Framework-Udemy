1235a9f7b6ef5f5527f816e62da69567f7d4e3aa
Luis Enrique Rojas Alvarado <lrojase1@gmail.com>
2022-06-06 23:16:24 -0500

    End to end purchase on a cart

--- SeleniumLearning/AddProductsToCart.cs
+++ SeleniumLearning/AddProductsToCart.cs
@@ -33,28 +33,47 @@ namespace SeleniumLearning
         [Test]
         public void AddingProductsToCart()
         {
-            String[] expectedProducts = { "iphone X", "BlackBerry" };
+            String[] expectedProducts = { "iphone X", "Blackberry" };
+            String[] actualProducts = new string[2];
             driver.FindElement(By.Id("username")).SendKeys("rahulshettyacademy");           
             driver.FindElement(By.Name("password")).SendKeys("learning");
             driver.FindElement(By.XPath("//div[@class='form-group'][5]/label/span/input")).Click(); 
             driver.FindElement(By.XPath("//input[@id='signInBtn']")).Click();
 
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(8));
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(45));
             wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.PartialLinkText("Checkout")));
 
             IList<IWebElement> products = driver.FindElements(By.TagName("app-card"));
 
             foreach(IWebElement product in products)
             {
-                // displaying list of products on page
-                TestContext.Progress.WriteLine(product.FindElement(By.CssSelector(".card-title a")).Text);
                 // doing matching with the expected on first list
                 if(expectedProducts.Contains(product.FindElement(By.CssSelector(".card-title a")).Text))
                 {
                     product.FindElement(By.CssSelector(".card-footer button")).Click();
                 }
             }
+            
+            driver.FindElement(By.PartialLinkText("Checkout")).Click();// checking cart
+            IList<IWebElement> checkoutCards = driver.FindElements(By.CssSelector("h4 a"));
 
+            for(int i=0; i<checkoutCards.Count; i++)
+            {
+                actualProducts[i] = checkoutCards[i].Text;
+            }
+            Assert.AreEqual(expectedProducts, actualProducts);
+
+            driver.FindElement(By.CssSelector(".btn-success")).Click();
+            driver.FindElement(By.Id("country")).SendKeys("ind");
+            // finishing the purchasing 
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.LinkText("India")));
+            driver.FindElement(By.LinkText("India")).Click();
+
+            driver.FindElement(By.CssSelector("label[for*='checkbox2']")).Click();
+            driver.FindElement(By.CssSelector("[value='Purchase']")).Click();
+            String confirmTextPurchase = driver.FindElement(By.CssSelector(".alert-success")).Text;
+
+            StringAssert.Contains("Success", confirmTextPurchase); // See if 'Success' is present on hole text confirmTextPurchase 
         }
         [TearDown]
         public void CloseBrowser()
