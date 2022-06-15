7dcf839cc9349700d04e76a168b21dba37a20a71
Luis Enrique Rojas Alvarado <lrojase1@gmail.com>
2022-06-14 22:56:46 -0500

    Implementing reporting methods as screenshots and report page

--- SeleniumFramework/Utilities/Base.cs
+++ SeleniumFramework/Utilities/Base.cs
@@ -7,6 +7,9 @@ using NUnit.Framework;
 using OpenQA.Selenium;
 using WebDriverManager.DriverConfigs.Impl;
 using OpenQA.Selenium.Chrome;
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using NUnit.Framework.Interfaces;
 
 namespace SeleniumFramework.Utilities
 {
@@ -16,10 +19,30 @@ namespace SeleniumFramework.Utilities
         // now adding thread to run test in parallel
         // we need to add 'driver.Value' in order to get the thread
         public ThreadLocal <IWebDriver> driver = new ThreadLocal<IWebDriver>();
+        // reporting 
+        public ExtentReports extent;
+        public ExtentTest test; // screenshots
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            string workingDirectory = Environment.CurrentDirectory;
+            string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+            String reportPath = projectDirectory + "//index.html";
+            
+            var htmlReporter = new ExtentHtmlReporter(reportPath);
+            extent = new ExtentReports();
+            
+            extent.AttachReporter(htmlReporter);
+            extent.AddSystemInfo("Host Name", "Local host");
+            extent.AddSystemInfo("Environment", "QA");
+            extent.AddSystemInfo("Username", "Luis Rojas");
+        }
 
         [SetUp]
         public void Setup()
         {
+            test = extent.CreateTest(TestContext.CurrentContext.Test.Name);
             TestContext.Progress.WriteLine("Initializing SetUp");
 
             new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
@@ -28,22 +51,42 @@ namespace SeleniumFramework.Utilities
 
             driver.Value.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
 
-
             driver.Value.Manage().Window.Maximize();
 
             driver.Value.Url = "https://rahulshettyacademy.com/loginpagePractise/";
         }
 
+        [TearDown]
+        public void CloseBrowser()
+        {            
+            // adding implementation for reporting test case failed
+            var status = TestContext.CurrentContext.Result.Outcome.Status;
+            var stackTrace = TestContext.CurrentContext.Result.StackTrace;
+            
+            DateTime time = DateTime.Now;
+            String fileName = "Screenshot_" + time.ToString("h_mm_ss") + ".png";
+            
+            if(status == TestStatus.Failed)
+            {
+                test.Fail("Test has failed", captureScreenShot(driver.Value, fileName));
+                test.Log(Status.Fail, "test failed with logthace" + stackTrace);
+            }
+
+            TestContext.Progress.WriteLine("Finishing");
+            
+            extent.Flush(); // release all objects 
+            driver.Value.Quit();
+        }
         public IWebDriver getDriver()
         {
             return driver.Value;
         }
 
-        [TearDown]
-        public void CloseBrowser()
-        {
-            TestContext.Progress.WriteLine("Finishing");
-            driver.Value.Quit();
+        public MediaEntityModelProvider captureScreenShot(IWebDriver driver, String screenShotName) {
+            ITakesScreenshot ts = (ITakesScreenshot)driver;
+            var screenshot = ts.GetScreenshot().AsBase64EncodedString;
+
+            return MediaEntityBuilder.CreateScreenCaptureFromBase64String(screenshot, screenShotName).Build();
         }
     }
 }
